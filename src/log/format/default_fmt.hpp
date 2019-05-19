e3e1330b7c8f04e9657735683aa83c41c4c673c6
Rainers Vorza <rainersvorza@gmail.com>
2019-05-19 22:48:10 +0300

    Split DefaultFormatter into .hpp and .cpp files

--- src/log/format/default_fmt.hpp
+++ src/log/format/default_fmt.hpp
@@ -17,15 +17,7 @@ namespace scar {
                 std::string end = col::ansi::reset + "\n";
 
             public:
-                DefaultFormatter() {
-                    logcolors[Off] = { col::ansi::reset, col::ansi::reset };
-                    logcolors[Trace] = { col::ansi::reset, col::ansi::reset };
-                    logcolors[Debug] = { col::ansi::reset, col::ansi::reset };
-                    logcolors[Info] = { col::ansi::reset, col::ansi::reset };
-                    logcolors[Warning] = { col::ansi::fg_yellow + col::ansi::bold, col::ansi::fg_yellow };
-                    logcolors[Error] = { col::ansi::fg_red + col::ansi::bold, col::ansi::fg_red };
-                    logcolors[Critical] = { col::ansi::bold + col::ansi::bg_red, col::ansi::bg_red };
-                }
+                DefaultFormatter();
                 ~DefaultFormatter() = default;
 
                 // level[code]: message
@@ -33,16 +25,7 @@ namespace scar {
                 //    |
                 // ln | bad code preview
                 //    |     ^^^^
-                void fmt(const Log& msg, ::fmt::memory_buffer& buff) const final override {
-                    // Add log level
-                    if (msg.level >= Warning) {
-                        auto lvl_txt = loglevels.at(msg.level);
-                        fmt_help::append_str(buff, col::with_color(logcolors.at(msg.level).lvl, lvl_txt + ": "));
-                    }
-                    fmt_help::append_str(buff, col::with_color(logcolors.at(msg.level).msg, msg.payload));
-
-                    fmt_help::append_str(buff, end);
-                }
+                void fmt(const Log& msg, ::fmt::memory_buffer& buff) const final override;
 
                 inline ColorPair get_lvl_color(LogLevel lvl) const {
                     return logcolors.at(lvl);
